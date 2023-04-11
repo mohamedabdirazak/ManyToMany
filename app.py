f6d8344eb43f3c2072679ef6af87536514b13a7e
mohamed madobe <mohamedmadobe@mohameds-MacBook-Pro.local>
2023-04-10 20:29:02 -0500

    exercise

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User, Post, Tag
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ihaveasecret'
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+
+@app.route('/')
+def root():
+
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+   
+
+    return render_template('404.html'), 404
